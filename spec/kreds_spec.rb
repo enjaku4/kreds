8f1733f36cf30868339d2dff381b51e49ae52e04
enjaku4 <enjaku4@gmail.com>
2025-06-03 20:38:25 +0200

    optimize specs

--- spec/kreds_spec.rb
+++ spec/kreds_spec.rb
@@ -1,370 +1,198 @@
 RSpec.describe Kreds do
   describe ".show" do
+    let(:credentials) do
+      {
+        foo: { bar: { baz: 42 } },
+        bad: nil,
+        secret_key_base: "dummy_secret_key_base",
+        test: { foo: [1, 2, 3] }
+      }
+    end
+
     it "returns the credentials structure" do
-      expect(described_class.show).to eq(
-        {
-          foo: {
-            bar: {
-              baz: 42
-            }
-          },
-          bad: nil,
-          secret_key_base: "dummy_secret_key_base",
-          test: {
-            foo: [1, 2, 3]
-          }
-        }
-      )
+      expect(described_class.show).to eq(credentials)
     end
   end
 
   describe ".fetch!" do
     describe "input validation" do
-      subject { described_class.fetch!(*args, var:) }
-
-      context "when no keys are provided" do
-        let(:args) { [] }
-        let(:var) { nil }
-
-        it "raises error" do
-          expect { subject }.to raise_error(Kreds::InvalidArgumentError, "[] violates constraints (min_size?(1, []) failed)")
-        end
+      it "raises error for empty keys" do
+        expect { described_class.fetch!([]) }.to raise_error(Kreds::InvalidArgumentError, /to_sym/)
       end
 
-      context "when keys are not symbols or strings" do
-        let(:args) { [:foo, 42] }
-        let(:var) { nil }
-
-        it "raises error" do
-          expect { subject }.to raise_error(Kreds::InvalidArgumentError, /undefined method (`|')to_sym' for (42:|an instance of )Integer/)
-        end
-      end
-
-      context "when var is not a string" do
-        let(:args) { [:foo, :bar, :bad] }
-        let(:var) { 42 }
-
-        it "raises error" do
-          expect { subject }.to raise_error(Kreds::InvalidArgumentError, "42 violates constraints (type?(String, 42) failed)")
-        end
+      it "raises error for invalid key types" do
+        expect { described_class.fetch!(:foo, 42) }.to raise_error(Kreds::InvalidArgumentError, /to_sym/)
       end
     end
 
-    context "when var is not provided" do
-      subject { described_class.fetch!(*args) }
-
-      context "when symbol keys exist and the value is in place" do
-        let(:args) { [:foo, :bar, :baz] }
-
-        it { is_expected.to eq(42) }
+    context "without environment variable fallback" do
+      it "returns value for symbol keys" do
+        expect(described_class.fetch!(:foo, :bar, :baz)).to eq(42)
       end
 
-      context "when string keys exist and the value is in place" do
-        let(:args) { ["foo", "bar", "baz"] }
-
-        it { is_expected.to eq(42) }
+      it "returns value for string keys" do
+        expect(described_class.fetch!("foo", "bar", "baz")).to eq(42)
       end
 
-      context "when a key is missing" do
-        let(:args) { [:foo, :bar, :bad] }
-
-        it "is raises error" do
-          expect { subject }.to raise_error(Kreds::UnknownCredentialsError, "Credentials key not found: [:foo][:bar][:bad]")
-        end
+      it "raises error for missing end key" do
+        expect { described_class.fetch!(:foo, :bar, :bad) }
+          .to raise_error(Kreds::UnknownCredentialsError, "Credentials key not found: [:foo][:bar][:bad]")
       end
 
-      context "when a key in the middle is missing" do
-        let(:args) { [:foo, :bad, :baz] }
-
-        it "raises error" do
-          expect { subject }.to raise_error(Kreds::UnknownCredentialsError, "Credentials key not found: [:foo][:bad]")
-        end
+      it "raises error for missing middle key" do
+        expect { described_class.fetch!(:foo, :bad, :baz) }
+          .to raise_error(Kreds::UnknownCredentialsError, "Credentials key not found: [:foo][:bad]")
       end
 
-      context "when the value is blank" do
-        let(:args) { [:bad] }
-
-        it "raises error" do
-          expect { subject }.to raise_error(Kreds::BlankCredentialsError, "Blank value in credentials: [:bad]")
-        end
-      end
-
-      context "when there are too many keys" do
-        let(:args) { [:foo, :bar, :baz, :bad] }
-
-        it "raises error" do
-          expect { subject }.to raise_error(Kreds::UnknownCredentialsError, "Credentials key not found: [:foo][:bar][:baz][:bad]")
-        end
+      it "raises error for blank value" do
+        expect { described_class.fetch!(:bad) }
+          .to raise_error(Kreds::BlankCredentialsError, "Blank value in credentials: [:bad]")
       end
     end
 
-    context "when var is provided" do
-      subject { described_class.fetch!(*args, var:) }
+    context "with environment variable fallback" do
+      it "raises error for invalid var type when fallback is needed" do
+        expect { described_class.fetch!(:bad, var: 42) }.to raise_error(Kreds::InvalidArgumentError, /type\?\(String/)
+      end
 
-      context "when the environment variable exists and the value is in place" do
-        let(:var) { "RAILS_ENV" }
-
-        context "when the credential are in place" do
-          let(:args) { [:foo, :bar, :baz] }
-
-          it { is_expected.to eq(42) }
+      context "when env var exists" do
+        it "returns credentials when available" do
+          expect(described_class.fetch!(:foo, :bar, :baz, var: "RAILS_ENV")).to eq(42)
         end
 
-        context "when the credential is blank" do
-          let(:args) { [:bad] }
-
-          it { is_expected.to eq("test") }
+        it "returns env var when credentials blank" do
+          expect(described_class.fetch!(:bad, var: "RAILS_ENV")).to eq("test")
         end
 
-        context "when the credential is missing" do
-          let(:args) { [:foo, :bar, :bad] }
-
-          it { is_expected.to eq("test") }
+        it "returns env var when credentials missing" do
+          expect(described_class.fetch!(:foo, :bar, :bad, var: "RAILS_ENV")).to eq("test")
         end
       end
 
-      context "when the environment variable is missing" do
-        let(:var) { "MISSING_ENV_VAR" }
-
-        context "when credentials are in place" do
-          let(:args) { [:foo, :bar, :baz] }
-
-          it { is_expected.to eq(42) }
+      context "when env var missing" do
+        it "returns credentials when available" do
+          expect(described_class.fetch!(:foo, :bar, :baz, var: "MISSING_VAR")).to eq(42)
         end
 
-        context "when the credential is blank" do
-          let(:args) { [:bad] }
-
-          it "raises error" do
-            expect { subject }.to raise_error(Kreds::Error, "Blank value in credentials: [:bad], Environment variable not found: \"MISSING_ENV_VAR\"")
-          end
+        it "raises combined error for blank credentials" do
+          expect { described_class.fetch!(:bad, var: "MISSING_VAR") }
+            .to raise_error(Kreds::Error, /Blank value in credentials.*Environment variable not found/)
         end
 
-        context "when the credential is missing" do
-          let(:args) { [:foo, :bar, :bad] }
-
-          it "raises error" do
-            expect { subject }.to raise_error(Kreds::Error, "Credentials key not found: [:foo][:bar][:bad], Environment variable not found: \"MISSING_ENV_VAR\"")
-          end
+        it "raises combined error for missing credentials" do
+          expect { described_class.fetch!(:foo, :bar, :bad, var: "MISSING_VAR") }
+            .to raise_error(Kreds::Error, /Credentials key not found.*Environment variable not found/)
         end
       end
 
-      context "when the environment variable value is blank" do
-        let(:var) { "BLANK_ENV_VAR" }
-
-        before { ENV["BLANK_ENV_VAR"] = "" }
-        after { ENV.delete("BLANK_ENV_VAR") }
-
-        context "when credentials are in place" do
-          let(:args) { [:foo, :bar, :baz] }
-
-          it { is_expected.to eq(42) }
+      context "when env var blank" do
+        around do |example|
+          ENV["BLANK_VAR"] = ""
+          example.run
+          ENV.delete("BLANK_VAR")
         end
 
-        context "when the credential is blank" do
-          let(:args) { [:bad] }
-
-          it "raises error" do
-            expect { subject }.to raise_error(Kreds::Error, "Blank value in credentials: [:bad], Blank value in environment variable: \"BLANK_ENV_VAR\"")
-          end
+        it "returns credentials when available" do
+          expect(described_class.fetch!(:foo, :bar, :baz, var: "BLANK_VAR")).to eq(42)
         end
 
-        context "when the credential is missing" do
-          let(:args) { [:foo, :bar, :bad] }
-
-          it "raises error" do
-            expect { subject }.to raise_error(Kreds::Error, "Credentials key not found: [:foo][:bar][:bad], Blank value in environment variable: \"BLANK_ENV_VAR\"")
-          end
-        end
-      end
-    end
-
-    context "when the block is given" do
-      let(:args) { [:no_such_key] }
-      let(:var) { "MISSING_ENV_VAR" }
-
-      it "returns the block value" do
-        expect(described_class.fetch!(*args, var:) { "default_value" }).to eq("default_value")
-      end
-    end
-  end
-
-  describe ".env_fetch!" do
-    describe "input validation" do
-      subject { described_class.env_fetch!(*args, var:) }
-
-      context "when keys are not symbols or strings" do
-        let(:args) { [:foo, 42] }
-        let(:var) { nil }
-
-        it "raises error" do
-          expect { subject }.to raise_error(Kreds::InvalidArgumentError, /undefined method (`|')to_sym' for (42:|an instance of )Integer/)
-        end
-      end
-
-      context "when var is not a string" do
-        let(:args) { [:foo, :bar, :baz] }
-        let(:var) { 42 }
-
-        it "raises error" do
-          expect { subject }.to raise_error(Kreds::InvalidArgumentError, "42 violates constraints (type?(String, 42) failed)")
-        end
-      end
-    end
-
-    context "when no var is provided" do
-      subject { described_class.env_fetch!(*args) }
-
-      context "when no keys are provided" do
-        let(:args) { [] }
-
-        it { is_expected.to eq({ foo: [1, 2, 3] }) }
-      end
-
-      context "when keys exist and the value is in place" do
-        let(:args) { [:foo] }
-
-        it { is_expected.to eq([1, 2, 3]) }
-      end
-
-      context "when a key is missing" do
-        let(:args) { [:foo, :bar, :bad] }
-
-        it "raises error" do
-          expect { subject }.to raise_error(Kreds::UnknownCredentialsError, "Credentials key not found: [:test][:foo][:bar]")
-        end
-      end
-    end
-
-    context "when var is provided" do
-      subject { described_class.env_fetch!(*args, var:) }
-
-      context "when the environment variable exists and the value is in place" do
-        let(:var) { "RAILS_ENV" }
-
-        context "when the credential are in place" do
-          let(:args) { [:foo] }
-
-          it { is_expected.to eq([1, 2, 3]) }
-        end
-
-        context "when the credential is blank" do
-          let(:args) { [:bad] }
-
-          it { is_expected.to eq("test") }
-        end
-
-        context "when the credential is missing" do
-          let(:args) { [:foo, :bar, :bad] }
-
-          it { is_expected.to eq("test") }
-        end
-      end
-
-      context "when the environment variable is missing" do
-        let(:var) { "MISSING_ENV_VAR" }
-
-        context "when credentials are in place" do
-          let(:args) { [:foo] }
-
-          it { is_expected.to eq([1, 2, 3]) }
-        end
-
-        context "when the credential is blank" do
-          let(:args) { [:bad] }
-
-          it "raises error" do
-            expect { subject }.to raise_error(Kreds::Error, "Credentials key not found: [:test][:bad], Environment variable not found: \"MISSING_ENV_VAR\"")
-          end
-        end
-
-        context "when the credential is missing" do
-          let(:args) { [:foo, :bar, :bad] }
-
-          it "raises error" do
-            expect { subject }.to raise_error(Kreds::Error, "Credentials key not found: [:test][:foo][:bar], Environment variable not found: \"MISSING_ENV_VAR\"")
-          end
-        end
-      end
-
-      context "when the environment variable value is blank" do
-        let(:var) { "BLANK_ENV_VAR" }
-
-        before { ENV["BLANK_ENV_VAR"] = "" }
-        after { ENV.delete("BLANK_ENV_VAR") }
-
-        context "when credentials are in place" do
-          let(:args) { [:foo] }
-
-          it { is_expected.to eq([1, 2, 3]) }
-        end
-
-        context "when the credential is blank" do
-          let(:args) { [:bad] }
-
-          it "raises error" do
-            expect { subject }.to raise_error(Kreds::Error, "Credentials key not found: [:test][:bad], Blank value in environment variable: \"BLANK_ENV_VAR\"")
-          end
+        it "raises combined error for blank credentials" do
+          expect { described_class.fetch!(:bad, var: "BLANK_VAR") }
+            .to raise_error(Kreds::Error, /Blank value in credentials.*Blank value in environment variable/)
         end
       end
     end
 
     context "with block" do
-      it "returns the value from block" do
-        expect(described_class.env_fetch!(:no_such_key, var: "MY_VAR") { "default_value" }).to eq("default_value")
+      it "returns block value when both credentials and env var fail" do
+        result = described_class.fetch!(:missing, var: "MISSING_VAR") { "default" }
+        expect(result).to eq("default")
+      end
+    end
+
+    context "when handling edge cases" do
+      it "raises error when trying to access non-hash as hash" do
+        expect { described_class.fetch!(:foo, :bar, :baz, :extra) }
+          .to raise_error(Kreds::UnknownCredentialsError, "Credentials key not found: [:foo][:bar][:baz][:extra]")
+      end
+
+      it "works with single key" do
+        expect(described_class.fetch!(:foo)).to eq({ bar: { baz: 42 } })
+      end
+    end
+  end
+
+  describe ".env_fetch!" do
+    it "raises error for invalid key types" do
+      expect { described_class.env_fetch!(:foo, 42) }.to raise_error(Kreds::InvalidArgumentError, /to_sym/)
+    end
+
+    it "raises error for invalid var type when fallback is needed" do
+      expect { described_class.env_fetch!(:bad, var: 42) }.to raise_error(Kreds::InvalidArgumentError, /type\?\(String/)
+    end
+
+    context "without var" do
+      it "returns entire test environment hash when no keys provided" do
+        expect(described_class.env_fetch!).to eq({ foo: [1, 2, 3] })
+      end
+
+      it "returns specific value from test environment" do
+        expect(described_class.env_fetch!(:foo)).to eq([1, 2, 3])
+      end
+
+      it "raises error for missing key in test environment" do
+        expect { described_class.env_fetch!(:foo, :bar, :bad) }
+          .to raise_error(Kreds::UnknownCredentialsError, "Credentials key not found: [:test][:foo][:bar]")
+      end
+    end
+
+    context "with var" do
+      it "works like regular fetch! with environment prefix" do
+        expect(described_class.env_fetch!(:foo, var: "RAILS_ENV")).to eq([1, 2, 3])
+      end
+
+      it "falls back to env var when credentials missing" do
+        expect(described_class.env_fetch!(:missing, var: "RAILS_ENV")).to eq("test")
+      end
+    end
+
+    context "with block" do
+      it "returns block value when both fail" do
+        result = described_class.env_fetch!(:missing, var: "MISSING_VAR") { "default" }
+        expect(result).to eq("default")
       end
     end
   end
 
   describe ".var!" do
-    subject { described_class.var!(var) }
-
-    context "when no var is provided" do
-      let(:var) { nil }
-
-      it "raises error" do
-        expect { subject }.to raise_error(Kreds::InvalidArgumentError, "nil violates constraints (type?(String, nil) failed)")
-      end
+    it "raises error for nil var" do
+      expect { described_class.var!(nil) }.to raise_error(Kreds::InvalidArgumentError, /type\?\(String/)
     end
 
-    context "when var is not a string" do
-      let(:var) { 42 }
-
-      it "raises error" do
-        expect { subject }.to raise_error(Kreds::InvalidArgumentError, "42 violates constraints (type?(String, 42) failed)")
-      end
+    it "raises error for non-string var" do
+      expect { described_class.var!(42) }.to raise_error(Kreds::InvalidArgumentError, /type\?\(String/)
     end
 
-    context "when the environment variable exists and the value is in place" do
-      let(:var) { "RAILS_ENV" }
-
-      it { is_expected.to eq("test") }
+    it "returns environment variable value" do
+      expect(described_class.var!("RAILS_ENV")).to eq("test")
     end
 
-    context "when the environment variable is missing" do
-      let(:var) { "MISSING_ENV_VAR" }
-
-      it "raises error" do
-        expect { subject }.to raise_error(Kreds::UnknownEnvironmentVariableError, "Environment variable not found: \"MISSING_ENV_VAR\"")
-      end
+    it "raises error for missing variable" do
+      expect { described_class.var!("MISSING_VAR") }
+        .to raise_error(Kreds::UnknownEnvironmentVariableError, /Environment variable not found/)
     end
 
-    context "when the environment variable value is blank" do
-      let(:var) { "BLANK_ENV_VAR" }
-
-      before { ENV["BLANK_ENV_VAR"] = "" }
-      after { ENV.delete("BLANK_ENV_VAR") }
-
-      it "raises error" do
-        expect { subject }.to raise_error(Kreds::BlankEnvironmentVariableError, "Blank value in environment variable: \"BLANK_ENV_VAR\"")
-      end
+    it "raises error for blank variable" do
+      ENV["BLANK_VAR"] = ""
+      expect { described_class.var!("BLANK_VAR") }
+        .to raise_error(Kreds::BlankEnvironmentVariableError, /Blank value in environment variable/)
+      ENV.delete("BLANK_VAR")
     end
 
-    context "when the block is given" do
-      let(:var) { "MISSING_ENV_VAR" }
-
-      it "returns the block value" do
-        expect(described_class.var!(var) { "default_value" }).to eq("default_value")
+    context "with block" do
+      it "returns block value for missing variable" do
+        result = described_class.var!("MISSING_VAR") { "default" }
+        expect(result).to eq("default")
       end
     end
   end
