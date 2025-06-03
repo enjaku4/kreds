e032fe0cd7669e469179470b5614991c7db2490f
enjaku4 <enjaku4@gmail.com>
2025-06-03 20:55:35 +0200

    minor refactoring

--- lib/kreds/fetch.rb
+++ lib/kreds/fetch.rb
@@ -18,38 +18,34 @@ module Kreds
     end
 
     def var!(var, &)
-      check_var(Kreds::Inputs.process(var, as: :string), &)
+      value = ENV.fetch(Kreds::Inputs.process(var, as: :string))
+
+      return raise_or_yield(Kreds::BlankEnvironmentVariableError.new("Blank value in environment variable: #{var.inspect}"), &) if value.blank?
+
+      value
+    rescue KeyError
+      raise_or_yield(Kreds::UnknownEnvironmentVariableError.new("Environment variable not found: #{var.inspect}"), &)
     end
 
     private
 
     def fetch_key(hash, key, path, keys)
-      value = hash.fetch(key) do
-        raise Kreds::UnknownCredentialsError, "Credentials key not found: #{path_to_s(path)}"
-      end
+      value = hash.fetch(key)
 
       raise Kreds::BlankCredentialsError, "Blank value in credentials: #{path_to_s(path)}" if value.blank?
       raise Kreds::UnknownCredentialsError, "Credentials key not found: #{path_to_s(path)}[:#{keys[path.size]}]" unless value.is_a?(Hash) || keys == path
 
       value
+    rescue KeyError
+      raise Kreds::UnknownCredentialsError, "Credentials key not found: #{path_to_s(path)}"
     end
 
     def fallback_to_var(error, var, &)
       return raise_or_yield(error, &) if var.blank?
 
-      check_var(var, &)
+      var!(var, &)
     rescue Kreds::BlankEnvironmentVariableError, Kreds::UnknownEnvironmentVariableError => e
       raise_or_yield(Kreds::Error.new("#{error.message}, #{e.message}"), &)
-    end
-
-    def check_var(var, &)
-      value = ENV.fetch(var) do
-        return raise_or_yield(Kreds::UnknownEnvironmentVariableError.new("Environment variable not found: #{var.inspect}"), &)
-      end
-
-      return raise_or_yield(Kreds::BlankEnvironmentVariableError.new("Blank value in environment variable: #{var.inspect}"), &) if value.blank?
-
-      value
     end
 
     def raise_or_yield(error, &)
