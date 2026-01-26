0c4806728825cc685c95015b6f5d1bf595066154
enjaku4 <enjaku4@gmail.com>
2026-01-26 22:27:30 +0100

    Update spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,10 +8,6 @@ RSpec.configure do |config|
   config.example_status_persistence_file_path = ".rspec_status"
 
   config.disable_monkey_patching!
-
-  config.expect_with :rspec do |c|
-    c.syntax = :expect
-  end
 end
 
 require "#{File.dirname(__FILE__)}/support/test_app"
